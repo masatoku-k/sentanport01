0aa063bf66320d7b965c5335f9fa5f837617a3ff
MarkN <59971459+masatoku-k@users.noreply.github.com>
2024-12-03 21:13:25 +0900

    Update picture.js
    一部コメントアウトを削除

--- picture.js
+++ picture.js
@@ -6,17 +6,7 @@ import mito from '../images/mito.gif'
 import icon from '../images/icon.jpg'
 import back from '../images/back.png'
 import Link from 'next/link'
-/*
-git config user.email "akaharaimori2000@gmail.com"git config user.name "masatoku-k"
-git add -A
-git commit -m “first commit”
-git push origin main
-git remote add origin https://github.com/masatoku-k/my-portfolio.git
-npm run dev
-C:\Users\ia21021\my-portfolio
-npm run dev   ローカルで表示
-<Image src={real} width={200} height={200} alt="this is masatoku"/>
-<div classNameName={styles.main}>*/
+
 
 export default function picture() {
   return (
